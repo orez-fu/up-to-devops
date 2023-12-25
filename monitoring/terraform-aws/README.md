0c3ff2b6e6750ac4a91b5d8fdaf56c15a5d7f621
Hoang Phu <phuhoang.c2@gmail.com>
2023-12-25 12:01:45 +0700

    init

---
+++ monitoring/terraform-aws/README.md
@@ -0,0 +1,9 @@
+# Terraform provisioning infrastructure
+
+```bash
+terraform int
+terraform validate
+terraform plan --var-file=./dev.tfvars
+terraform apply --var-file=./dev.tfvars
+
+```
---
+++ monitoring/terraform-aws/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.region
+}
+
+module "ec2" {
+  source             = "./modules/ec2"
+  region             = var.region
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  instance_count     = var.instance_count
+  name               = var.name
+  environment        = var.environment
+  application        = var.application
+  security_group_ids = module.security-group.security_group_ids
+  volume-size        = var.volume-size
+}
+
+module "security-group" {
+  source      = "./modules/security-group"
+  region      = var.region
+  tags        = var.tags
+  name        = var.name
+  environment = var.environment
+  application = var.application
+
+  ingress_cidr_from_port   = var.ingress_cidr_from_port
+  ingress_cidr_to_port     = var.ingress_cidr_to_port
+  ingress_cidr_protocol    = var.ingress_cidr_protocol
+  ingress_cidr_block       = var.ingress_cidr_block
+  ingress_cidr_description = var.ingress_cidr_description
+  create_ingress_cidr      = var.create_ingress_cidr
+
+  egress_cidr_from_port = var.egress_cidr_from_port
+  egress_cidr_to_port   = var.egress_cidr_to_port
+  egress_cidr_protocol  = var.egress_cidr_protocol
+  egress_cidr_block     = var.egress_cidr_block
+  create_egress_cidr    = var.create_egress_cidr
+
+}
